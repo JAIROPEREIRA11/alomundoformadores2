248778735e07a2b1ab0b8050ce87b7c1fae6e0a7
JAIROPEREIRA11 <105813968+JAIROPEREIRA11@users.noreply.github.com>
2025-07-01 16:19:51 -0300

    atualizando script

---
+++ script.js
@@ -0,0 +1,13 @@
+function inserirNome(){
+   let nomeUsuario = prompt("Qual o seu nome");
+   let elemento = document.querySelector("#nome-usuario");
+   console.log(elemento);
+   elemento.textContent = nomeUsuario;
+}
+
+inserirNome();
+
+let linguagens = ["JavaScript", "Python", "C++"]
+console.log (linguagens [0]);
+console.log (linguagens [1]);
+console.log (linguagens [2]);
