b55509193dc36461fd3faf79688592f418499d3e
JAIROPEREIRA11 <105813968+JAIROPEREIRA11@users.noreply.github.com>
2025-07-29 16:54:14 -0300

    .

--- script.js
+++ script.js
@@ -19,3 +19,20 @@ lista.textContent = linguagens[n];
 lista.textContent = linguagens [0];
 lista.textContent = linguagens [1];
 lista.textContent = linguagens [2];
+
+let aluno1 = {
+  nome: "Jairo",
+  idade: 39,
+  anoLetivo: "Universidade",
+  materiasFavoritas: ["Matematica","Programação","Arte"]
+}
+
+console.log(aluno1.nome); 
+console.log(aluno1.idade);
+console.log(aluno1.materiasFavoritas);
+console.log(aluno1.materiasFavoritas[1]);
+
+const objeto = document.querySelector("#aluno");
+objeto.textContent = aluno1.nome;
+objeto.textContent = aluno1.idade;
+objeto.textContent = aluno1.materiasFavoritas[1];
