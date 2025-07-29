9548c0866c5a78a9a2b9ae1b333a6f064810c562
JAIROPEREIRA11 <105813968+JAIROPEREIRA11@users.noreply.github.com>
2025-07-29 16:25:03 -0300

    .

--- script.js
+++ script.js
@@ -12,7 +12,10 @@ console.log (linguagens [0]);
 console.log (linguagens [1]);
 console.log (linguagens [2]);
 
+
+
 const lista = document.querySelector ("#lista");
+lista.textContent = linguagens[n];
 lista.textContent = linguagens [0];
 lista.textContent = linguagens [1];
 lista.textContent = linguagens [2];
